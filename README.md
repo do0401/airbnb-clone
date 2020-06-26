43330811c8b018c65df3218c94ecc5b7ab1e0ff8
nmwh <43128952+do0401@users.noreply.github.com>
2020-06-26 14:10:17 +0900

    #4.3 완료

---
+++ README.md
@@ -0,0 +1,3 @@
+# notes
+## 1. 장고에 있는 모든 class들은 string method를 가지고 있다.
+- 파이썬이 class를 발견하면 class를 마치 string 처럼 보여주는데 이 method를 파이썬에서 `__str__` 이라고 표시한다.
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,24 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract: True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    
+    pass
+
+
 # Create your models here.
 class Room(core_models.TimeStampedModel):
 
@@ -28,3 +46,7 @@ class Room(core_models.TimeStampedModel):
     # user는 foreign key가 필요없기 때문에 따로 필드를 가지고 있지 않다.
     # 해당 내용은 #4.2 참조
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
