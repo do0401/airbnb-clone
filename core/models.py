ea4204f86f8f79e87531693a2c677ecf8afa6ee6
nmwh <43128952+do0401@users.noreply.github.com>
2020-06-26 11:45:45 +0900

    #4.2 완료

--- core/models.py
+++ core/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    # 장고가 새로운 모델을 만들면 현재 날짜와 시간을 여기에 넣어준다.
+    created = models.DateTimeField(auto_now_add=True)
+    # 모델을 저장할 때마다 항상 새로운 날짜를 써준다.
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
       abstract = True
--- rooms/models.py
+++ rooms/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
 
 
 # Create your models here.
@@ -7,4 +9,22 @@ class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
 
-    pass
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    # foreign key는 한 모델을 다른 모델과 연결시켜주는 역할을 한다.
+    # foreign key는 일대다 관계이다.
+    # 100개의 룸을 만들면 그 룸은 한 user를 가리킬 것이다.
+    # user는 foreign key가 필요없기 때문에 따로 필드를 가지고 있지 않다.
+    # 해당 내용은 #4.2 참조
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
